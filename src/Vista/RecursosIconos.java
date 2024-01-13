8c4476f5843c4d12f565321c36cc13d83883e5b5
dominguez pablo maximiliano <dominguez2020@outlook.com.ar>
2024-01-13 00:51:51 -0300

    cambio funcionamiento label

--- src/Vista/RecursosIconos.java
+++ src/Vista/RecursosIconos.java
@@ -1,6 +1,5 @@
 package Vista;
 
-
 import javax.swing.ImageIcon;
 import java.util.ArrayList;
 
@@ -18,23 +17,44 @@ public class RecursosIconos {
         // Rellenar las listas con los iconos correspondientes
         baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO ABAJO DERECHA.png"));
         baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO IZQUIERDA DERECHA ABAJO.png"));
+        baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO ABAJO IZQUIERDA.png"));
+        baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO ARRIBA ABAJO DERECHA.png"));
+        baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO ARRIBA ABAJO IZQUIERDA.png"));
+        baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO ARRIBA DERECHA.png"));
+        baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO ARRIBA IZQUIERDA.png"));
+        baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO IZQUIERDA DERECHA ARRIBA.png"));
+        baseIconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\BCIRCULO TOTAL.png"));
 
-        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO ABAJO DERECHA.png"));  // Reemplaza "rutaJ1_1" con la ruta real de tu icono
-        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO IZQUIERDA DERECHA ABAJO.png"));  // Reemplaza "rutaJ1_2" con la ruta real de tu icono
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO ABAJO DERECHA.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO IZQUIERDA DERECHA ABAJO.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO ABAJO IZQUIERDA.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO ARRIBA ABAJO DERECHA.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO ARRIBA ABAJO IZQUIERDA.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO ARRIBA DERECHA.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO ARRIBA IZQUIERDA.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO IZQUIERDA DERECHA ARRIBA.png"));
+        j1Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J1CIRCULO TOTAL.png"));
 
-        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO ABAJO DERECHA.png"));  // Reemplaza "rutaJ2_1" con la ruta real de tu icono
-        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO IZQUIERDA DERECHA ABAJO.png"));  // Reemplaza "rutaJ2_2" con la ruta real de tu icono
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO ABAJO DERECHA.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO IZQUIERDA DERECHA ABAJO.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO ABAJO IZQUIERDA.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO ARRIBA ABAJO DERECHA.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO ARRIBA ABAJO IZQUIERDA.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO ARRIBA DERECHA.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO ARRIBA IZQUIERDA.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO IZQUIERDA DERECHA ARRIBA.png"));
+        j2Iconos.add(new ImageIcon("C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\J2CIRCULO TOTAL.png"));
     }
 
-    public ArrayList<ImageIcon> getBaseIcono() {
+    public ArrayList<ImageIcon> getBaseIconos() {
         return baseIconos;
     }
 
-    public ArrayList<ImageIcon> getJ1Icono() {
+    public ArrayList<ImageIcon> getJ1Iconos() {
         return j1Iconos;
     }
 
-    public ArrayList<ImageIcon> getJ2Icono() {
+    public ArrayList<ImageIcon> getJ2Iconos() {
         return j2Iconos;
     }
 }
--- src/Vista/VistaGrafica.java
+++ src/Vista/VistaGrafica.java
@@ -12,33 +12,75 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 
-public class VistaGrafica extends JFrame implements Ivista{private JPanel VG;
+public class VistaGrafica extends JFrame implements Ivista{
+    private JPanel VG;
     private JLabel l00;
     private JLabel l03;
     private JButton botonInicio;
     private JLabel caero;
+    private JLabel l06;
+    private JLabel l11;
+    private JLabel l13;
+    private JLabel l15;
+    private JLabel l22;
+    private JLabel l24;
+    private JLabel l35;
+    private JLabel l31;
+    private JLabel l30;
+    private JLabel l36;
+    private JLabel l32;
+    private JLabel l34;
+    private JLabel l23;
+    private JLabel l60;
+    private JLabel l43;
+    private JLabel l53;
     private Controlador c;
-    ArrayList<ImageIcon> Baseiconos = new RecursosIconos().getBaseIcono();
-    ArrayList<ImageIcon> j1iconos = new RecursosIconos().getJ1Icono();
-    ArrayList<ImageIcon> j2iconos = new RecursosIconos().getJ2Icono();
+    ArrayList<ImageIcon> Baseiconos = new RecursosIconos().getBaseIconos();
+    ArrayList<ImageIcon> j1iconos = new RecursosIconos().getJ1Iconos();
+    ArrayList<ImageIcon> j2iconos = new RecursosIconos().getJ2Iconos();
     ArrayList<JLabel> labels = new ArrayList<>();
 
 
     public VistaGrafica(Controlador c) {
-        caero.setVisible(false);
         this.c = c;
         c.setVista(this);
         setLocationRelativeTo(null);
         setSize(600, 600);
         setContentPane(VG);
 
-        // Agregar el MouseListener a los JLabels
+        l00.setName("0");
+        l11.setName("0");
+        l22.setName("0");
+        l03.setName("1");
+        l43.setName("1");
+        l06.setName("2");
+        l15.setName("2");
+        l24.setName("2");
+        l53.setName("8");
+        l13.setName("8"); //los del 8 no andan
+        l31.setName("8");
+        l35.setName("8");
+
         labels.add(l00);
+        labels.add(l11);
+        labels.add(l22);
         labels.add(l03);
+        labels.add(l43);
+        labels.add(l06);
+        labels.add(l13);
+        labels.add(l15);
+        labels.add(l24);
+        labels.add(l35);
+        labels.add(l31);
+        labels.add(l30);
+        labels.add(l36);
+        labels.add(l32);
+        labels.add(l34);
+        labels.add(l23);
 
-        l00.addMouseListener(new LabelClickListener());
-        l03.addMouseListener(new LabelClickListener());
-
+        for (JLabel label : labels) {
+            label.addMouseListener(new LabelClickListener());
+        }
 
         botonInicio.addActionListener(new ActionListener() {
             @Override
@@ -57,21 +99,22 @@ public class VistaGrafica extends JFrame implements Ivista{private JPanel VG;
         int turno = c.getTurno();
         setBackground(Color.lightGray);
         caero.setVisible(true);
+        caero.setText(String.valueOf(turno));
         if (turno == 1){
             setBackground(Color.white);
-            caero.setIcon(new ImageIcon( "C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\j1icono.png"));
-            for(int i = 0;i< labels.size();i++){
+            //caero.setIcon(new ImageIcon( "C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\j1icono.png"));
+            for(int i = 0;i< Baseiconos.size();i++){ // hay mas label que iconos
                 if (Objects.equals(labels.get(i).getText(), casilla)){
-                    labels.get(i).setIcon(j1iconos.get(i));
+                    labels.get(i).setIcon(j1iconos.get(Integer.parseInt(labels.get(i).getName())));
                 }
             }
         }else{
 
-            for(int i = 0;i< labels.size();i++){
+            for(int i = 0;i< Baseiconos.size();i++){
                 setBackground(Color.white);
-                caero.setIcon(new ImageIcon( "C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\j2icono.png"));
+                //caero.setIcon(new ImageIcon( "C:\\Users\\domin\\OneDrive\\Escritorio\\molino-main\\src\\recursos\\j2icono.png"));
                 if (Objects.equals(labels.get(i).getText(), casilla)){
-                    labels.get(i).setIcon(j2iconos.get(i));
+                    labels.get(i).setIcon(j2iconos.get(Integer.parseInt(labels.get(i).getName())));
                 }
             }
         }
@@ -99,3 +142,4 @@ public class VistaGrafica extends JFrame implements Ivista{private JPanel VG;
         }
     }
 }
+
